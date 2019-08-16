995665586cdc0b807115a7ba1835330f7f2aae2a
reeshkeed <girishdaloso@hotmail.com>
2019-08-16 16:49:51 +0800

    Resize Rizal family pictures

--- src/views/Biography.vue
+++ src/views/Biography.vue
@@ -8,9 +8,21 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
+      <div class="wrapper text-center">
+        <h3>Family of Rizal</h3>
+        <h6 class="text-muted">The Mercado - Rizal Family</h6>
+      </div>
+
+      <div class="card">
+        <img src="../../static/img/rizal_family/jose.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-center">JOSE RIZAL <br>
+            <span class="text-muted">(1861-1896)</span>
+          </h5>
+          <p class="card-text">The second son and the seventh child. He was executed by the Spaniards on December 30,1896.</p>
+        </div>
+      </div>
+
     </div>
   </div>
 
