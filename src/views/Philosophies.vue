e22e98cc04454af942deb7d181f78b65f8fd9c49
reeshkeed <girishdaloso@hotmail.com>
2019-08-17 06:49:18 +0800

    Added philosophies page content

--- src/views/Philosophies.vue
+++ src/views/Philosophies.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="philosophies">
-    <div class="jumbotron v-centered bg-philosophy">
+    <div class="jumbotron v-centered slant-left bg-philosophy">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Philosophies</h1>
@@ -8,10 +8,170 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
+      <div class="wrapper text-center">
+        <h3>Philosophies in Life</h3>
+      </div>
+
+      <div class="wrapper">
+        <p>
+          Philosophy may be defined as the study and pursuit of facts which deal with the ultimate reality or causes of things as they affect life.
+        </p>
+        <p>
+          The philosophy of a country like the Philippines is made up of the intricate and composite interrelationship of the life
+          histories of its people; in other word, the philosophy of our nation would be strange and undefinable if we do not delve into
+          the past tied up with the notable life experiences of the representative personalities of our nation.
+        </p>
+        <p>
+          Being one of the prominent representatives of Filipino personalities, Jose Rizal is a fit subject whose life philosophy deserves to be recognized. 
+        </p>
+        <p>
+          Having been a victim of Spanish brutality early in his life in Calamba, Rizal had thus already formed the nucleus of an unfavorable
+          opinion of Castillian imperialistic administration of his country and people.
+        </p>
+        <p>
+          Pitiful social conditions existed in the Philippines as late as three centuries after his conquest in Spain, with agriculture,
+          commerce, communications and education languishing under its most backward state. It was because of this social malady that social
+          evils like inferiority complex, cowardice, timidity and false pride pervaded nationally and contributed to the decay of social life.
+          This stimulated and shaped Rizal’s life phylosophy to be to contain if not eliminate these social ills.
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Educational Philosophy
+        </h6>
+        <p>
+          Rizal’s concept of the importance of education is clearly enunciated in his work entitled Instruction wherein he
+          sought improvements in the schools and in the methods of teaching. He maintained that the backwardness of his country
+          during the Spanish ear was not due to the Filipinos’ indifference, apathy or indolence as claimed by the rulers, but to the
+          neglect of the Spanish authorities in the islands. For Rizal, the mission of education is to elevate the country to the highest
+          seat of glory and to develop the people’s mentality. Since education is the foundation of society and a prerequisite for social progress,
+          Rizal claimed that only through education could the country be saved from domination.
+        </p>
+        <p>
+          Rizal’s philosophy of education, therefore, centers on the provision of proper motivation in order to bolster the great
+          social forces that make education a success, to create in the youth an innate desire to cultivate his intelligence and give him life eternal.
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Religious Philosophy
+        </h6>
+        <p>
+          Rizal grew up nurtured by a closely-knit Catholic family, was educated in the foremost Catholic schools of the period in the elementary,
+          secondary and college levels; logically, therefore, he should have been a propagator of strictly Catholic traditions. However,
+          in later life, he developed a life philosophy of a different nature, a philosophy of a different Catholic practice intermingled
+          with the use of Truth and Reason.
+        </p>
+        <p>Why the change?</p>
+        <p>
+          It could have been the result of contemporary contact, companionship, observation, research and the possession of an independent spirit.
+          Being a critical observer, a profound thinker and a zealous reformer, Rizal did not agree with the prevailing Christian propagation
+          of the Faith by fire and sword. This is shown in his Annotation of Morga’s Sucesos de las Islas Filipinas.
+        </p>
+        <p>
+          Rizal did not believe in the Catholic dogma that salvation was only for Catholics and that outside Christianity, salvation was not
+          possible even if Catholics composed only a small minority of the world’s religious groups. Nor did he believe in the Catholic observation
+          of fasting as a sacrifice, nor in the sale of such religious items as the cross, medals, rosaries and the like in order to propagate
+          the Faith and raise church funds. He also lambasted the superstitious beliefs propagated by the priests in the church and in the schools.
+          All of these and a lot more are evidences of Rizal’s religious philosophy. 
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Political Philosophy
+        </h6>
+        <p>
+          In Rizal’s political view, a conquered country like the Philippines should not be taken advantage of but rather should be developed,
+          civilized, educated and trained in the science of self-government.
+        </p>
+        <p>
+          He bitterly assailed and criticized in publications the apparent backwardness of the Spanish ruler’s method of governing the country
+          which resulted in: 
+        </p>
+        <ul>
+          <li>The bondage and slavery of the conquered</li>
+          <li>The Spanish government’s requirement of forced labor and force military service upon the n natives</li>
+          <li>The abuse of power by means of exploitation</li>
+          <li>The government ruling that any complaint against the authorities was criminal</li>
+          <li>And making the people ignorant, destitute and fanatic, thus discouraging the formation of a national sentiment</li>
+        </ul>
+        <p>
+          Rizal’s guiding political philosophy proved to be the study and application of reforms, the extension of human rights,
+          the training for self government and the arousing of spirit of discontent over oppression, brutality, inhumanity,
+          sensitiveness and self love.
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Ethical Philosophy
+        </h6>
+        <p>
+          The study of human behavior as to whether it is good or bad or whether it is right or wrong is that science upon which Rizal’s
+          ethical philosophy was based. The fact that the Philippines was under Spanish domination during Rizal’s time led him to subordinate
+          his philosophy to moral problems. This trend was much more needed at that time because the Spaniards and the Filipinos
+          had different and sometimes conflicting morals. The moral status of the Philippines during this period was one with
+          a lack of freedom, one with predominance of foreign masters, one with an imposition of foreign religious worship, devotion,
+          homage and racial habits. This led to moral confusion among the people, what with justice being stifled, limited or
+          curtailed and the people not enjoying any individual rights.
+        </p>
+        <p>
+          To bolster his ethical philosophy, Dr. Rizal had recognized not only the forces of good and evil, but also the tendencies towards
+          good and evil. As a result, he made use of the practical method of appealing to the better nature of the conquerors and of offering
+          useful methods of solving the moral problems of the conquered.
+        </p>
+        <p>
+          To support his ethical philosophy in life, Rizal:
+        </p>
+        <ul>
+          <li>Censured the friars for abusing the advantage of their position as spiritual leaders and the ignorance and fanaticism of the natives</li>
+          <li>Counseled the Filipinos not to resent a defect attributed to them but to accept same as reasonable and just</li>
+          <li>Advised the masses that the object of marriage was the happiness and love of the couple and not financial gain</li>
+          <li>Censured the priests who preached greed and wrong morality</li>
+          <li>Advised every one that love and respect for parents must be strictly observed</li>
+        </ul>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Social Philosophy
+        </h6>
+        <p>
+          That body of knowledge relating to society including the wisdom which man's experience in society has taught him is social philosophy.
+          The facts dealt with are principles involved in nation building and not individual social problems. The subject matter of this social
+          philosophy covers the problems of the whole race, with every problem having a distinct solution to bolster the people’s social knowledge.
+        </p>
+        <p>
+          Rizal’s social philosophy dealt with:
+        </p>
+        <ul>
+          <li>Man in society</li>
+          <li>Influential factors in human life</li>
+          <li>Racial problems</li>
+          <li>Social constant</li>
+          <li>Social justice</li>
+          <li>Social ideal</li>
+          <li>Poverty and wealth</li>
+          <li>Reforms</li>
+          <li>Youth and greatness</li>
+          <li>History and progress</li>
+          <li>Future Philippines</li>
+        </ul>
+        <p>
+          The above dealt with man’s evolution and his environment, explaining for the most part human behavior and capacities like his will to live;
+          his desire to possess happiness; the change of his mentality; the role of virtuous women in the guidance of great men;
+          the need for elevating and inspiring mission; the duties and dictates of man’s conscience; man’s need of practicing gratitude;
+          the necessity for consulting reliable people; his need for experience; his ability to deny; the importance of deliberation;
+          the voluntary offer of man’s abilities and possibilities; the ability to think, aspire and strive to rise; and the proper use of hearth,
+          brain and spirit-all of these combining to enhance the intricacies, beauty and values of human nature. All of the above
+          served as Rizal’s guide in his continuous effort to make over his beloved Philippines.
+        </p>
+      </div>
+
+
     </div>
   </div>
-
 </template>
