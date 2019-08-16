25f18547e7feea068b58aec1867a509d7164a186
reeshkeed <girishdaloso@hotmail.com>
2019-08-17 07:33:21 +0800

    Added qoutes page content

--- src/views/Quotes.vue
+++ src/views/Quotes.vue
@@ -8,10 +8,84 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+      <div class="wrapper text-center">
+        <h3>Quotes</h3>
+        <h6 class="text-muted">by Jose Rizal</h6>
+      </div>
 
+      <div class="wrapper">
+        <ol>
+          <li>One only dies once, and if one does not die well, a good opportunity is lost and will not present itself again.</li>
+          <li>I wish to show those who deny us Patriotism that we know how to die for our country and convictions.</li>
+          <li>He who does not know how to look back at where he came from will never get to his destination.</li>
+          <li>The youth is the hope of our future.</li>
+          <li>Filipinos don't realize that victory is the child of struggle, that joy blossoms from suffering, and redemption is a product of sacrifice.</li>
+          <li>To live is to be among men, and to be among men is to struggle, a struggle not only with them but with oneself; with their passions, but also with one's own.</li>
+          <li>No good water comes from a muddy spring. No sweet fruit comes from a bitter seed.</li>
+          <li>On this battlefield man has no better weapon than his intelligence, no other force but his heart.</li>
+          <li>He who would love much has also much to suffer.</li>
+        </ol>
+      </div>
+
+      <div class="wrapper text-center">
+        <h3>Trivia</h3>
+        <h6 class="text-muted">About Jose Rizal</h6>
+      </div>
+
+      <div class="wrapper">
+        <ol>
+          <li>
+            Did you know Rizal’s mother could have been an illegitimate child. In 1814, Rizal’s grandfather Lorenzo Alberto Alonso, then 24,
+            married 12-year-old Ilocana Paula Florentino. However, Ocampo wrote that years later, the same Lorenzo Alberto Alonso was spotted
+            in Biñan with Brigida Quintos. They bore five children, one of which was Teodora Alonso, Rizal’s mother. Strangely enough,
+            documents in Santa Cruz Church, Manila, are complete right down to the 18th century—except the volume that held Teodora Alonso’s
+            baptismal records. Rizal, being the only member of his family to use this surname instead of "Mercado," also thought he was an
+            illegitimate child. However, his family insisted on using "Rizal" to avoid association with brother Paciano Mercado, who was
+            involved in the Gomburza issue of 1872.
+          </li>
+          <li>
+            DID YOU KNOW THAT THERE ARE THREE SPECIES WERE NAMED AFTER RIZAL? - Its was Draco rizali, a species of flying dragon,
+            Rachophorous rizali, a species of toad and Apogonia rizali, a beetle species.
+          </li>
+          <li>
+            DID YOU KNOW Rizal was only five feet and three inches tall -Rizal was only five feet and three inches tall but had a
+            waistline of 25-26 inches. He also had a big head. His hats were 6 1/2 inches across and the interior measures eight inches from front to back.
+          </li>
+          <li>
+            DID YOU KNOW a side Noli Me Tangere and El Filibusterismo he has third novel? -Rizal had his third unfinished work or novel.
+            Known by historians as “Maka-misa,” this unfinished work was started by Rizal in 1892 in Hongkong. Maka-misa is not
+            actually a title of the novel but only a single chapter of the unfinished novel. He began writing it in Tagalog but gave up
+            and continued writing in Spanish.
+          </li>
+          <li>
+            DID YOU KNOW our national hero’s favorite fruits were lanzones and mangoes? -According to According to Faustino “Tinong” Alfon,
+            Rizal’s former cook in Dapitan. His meals back then usually consisted of three kinds of ulam. Rizal’s breakfast, on the other hand,
+            was usually comprised of hot chocolate, rice, and sardinas secas, commonly known now as tuyo.
+          </li>
+          <li>
+            DID YOU KNOW that Rizal was addressed by his European lover as the ‘little bad boy’? -Filipinos are truly honest with themselves
+            saying Rizal was not that handsome – and given the fact he was a man of medium height and small built. However,
+            Jose Rizal had his way with women – it might be his humor, knowledge or the way he present himself. On the series of
+            letters that were discovered to be love messages for Rizal, a Belgian woman named Suzanne wrote: ‘There will never be any
+            home in which you are so loved as that in Brussels. So, you little bad boy, hurry back.
+          </li>
+          <li>
+            DID YOU KNOW Rizal once won the lottery? -Rizal joined lottery and won one-third of the grand prize Php18,000.00.
+          </li>
+          <li>
+            DID YOU KNOW Rizal has last words before he was executed? - It was “consummatum est”, which means “It is finished".
+          </li>
+          <li>
+            DID YOU KNOW Rizal’s allowance when he lived in Madrid was Php50 a month. -When the family’s Laguna farm wasn’t doing good,
+            he was given only Php35 for his monthly expenses.
+          </li>
+          <li>
+            DID YOU KNOW Rizal liked to take light meals with wine? - at the Viva Madrid restaurant, also a favorite of journalist and
+            revolutionary Graciano Lopez Jaena.
+          </li>
+        </ol>
+      </div>
+    </div>
+
+  </div>
 </template>
