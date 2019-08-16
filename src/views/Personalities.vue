e5d7ecddd30f4016cc72e4d6b1212b4766df2588
reeshkeed <girishdaloso@hotmail.com>
2019-08-17 07:22:54 +0800

    Fix personalities navbar slant

--- src/views/Personalities.vue
+++ src/views/Personalities.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="personalities">
-    <div class="jumbotron v-centered bg-persona">
+    <div class="jumbotron v-centered slant-left bg-persona">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Personalities</h1>
