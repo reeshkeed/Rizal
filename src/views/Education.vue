fca8a8437aac2c03c90d1311bfc346a5a3f66bf8
reeshkeed <girishdaloso@hotmail.com>
2019-08-17 06:33:44 +0800

    Added education page content

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="education">
-    <div class="jumbotron v-centered bg-education">
+    <div class="jumbotron v-centered slant-left bg-education">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Education</h1>
@@ -8,10 +8,186 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
+      <div class="wrapper text-center">
+        <h3>Jose Rizal's Education</h3>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Early Education in Calamba and Biñan
+        </h6>
+        <p>
+          Rizal had his early education in Calamba and Biñan. It was a typical schooling that a son of an ilustrado family received during his time,
+          characterized by the four R’s- reading, writing, arithmetic, and religion. Instruction was rigid and strict. Knowledge was forced into the
+          minds of the pupils by means of the tedious memory method aided by the teacher’s whip. Despite the defects of the
+          Spanish system of elementary education, Rizal was able to acquire the necessary instruction preparatory for college work in Manila.
+          It may be said that Rizal, who was born a physical weakling, rose to become an intellectual giant not because of, but rather in spite of,
+          the outmoded and backward system of instruction obtaining in the Philippines during the last decades of Spanish regime.
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          The Hero’s First Teacher
+        </h6>
+        <p>
+          The first teacher of Rizal was his mother, who was a remarkable woman of good character and fine culture. On her lap, he learned
+          at the age of three the alphabet and the prayers. "My mother," wrote Rizal in his student memoirs, "taught me how to read and to say
+          haltingly the humble prayers which I raised fervently to God."
+        </p>
+        <p>
+          As tutor, Doña Teodora was patient, conscientious, and understanding. It was she who first discovered that her son had a talent for poetry.
+          Accordingly, she encouraged him to write poems. To lighten the monotony of memorizing the ABC’s and to stimulate her son’s imagination,
+          she related many stories.
+        </p>
+        <p>
+          As Jose grew older, his parents employed private tutors to give him lessons at home. The first was Maestro Celestino and the second,
+          Maestro Lucas Padua. Later, an old man named Leon Monroy, a former classmate of Rizal’s father, became the boy’s tutor. This old teacher
+          lived at the Rizal home and instructed Jose in Spanish and Latin. Unfortunately, he did not lived long. He died five months later.
+        </p>
+        <p>
+          After a Monroy’s death, the hero’s parents decided to send their gifted son to a private school in Biñan.
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Jose Goes to Biñan
+        </h6>
+        <p>
+          One Sunday afternoon in June , 1869, Jose, after kissing the hands of his parents and a tearful parting from his sister,
+          left Calamba for Biñan. He was accompanied by Paciano , who acted as his second father. The two brothers rode in a carromata,
+          reaching their destination after one and one-half hours’ drive. They proceeded to their aunt’s house, where Jose was to lodge.
+          It was almost night when they arrived, and the moon was about to rise.
+        </p>
+        <p>
+          That same night, Jose, with his cousin named Leandro, went sightseeing in the town. Instead of enjoying the sights,
+          Jose became depressed because of homesickness. "In the moonlight," he recounted, "I remembered my home town, my idolized mother,
+          and my solicitous sisters. Ah, how sweet to me was Calamba, my own town, in spite of the fact that was not as wealthy as Biñan."
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          First Day in Biñan School
+        </h6>
+        <p>
+          The next morning (Monday) Paciano brought his younger brother to the school of Maestro Justiniano Aquino Cruz.
+          The school was in the house of the teacher, which was a small nipa hut about 30 meters from the home of Jose’s aunt.mPaciano
+          knew the teacher quite well because he had been a pupil under him before. He introduced Jose to the teacher, after which he departed
+          to return to Calamba. Immediately, Jose was assigned his seat in the class. The teacher asked him:
+        </p>
+        <p>"Do you know Spanish?"</p>
+        <p>"A little, sir," replied the Calamba lad.</p>
+        <p>"Do you know Latin?"</p>
+        <p>"A little, sir."</p>
+        <p>
+          The boys in the class, especially Pedro, the teacher’s son laughed at Jose’s answers. The teacher sharply stopped all noises and
+          begun the lessons of the day. Jose described his teacher in Biñan as follows: "He was tall, thin, long-necked, with sharp nose and a
+          body slightly bent forward, and he used to wear a sinamay shirt, woven by the skilled hands of the women of Batangas. He knew by the
+          heart the grammars by Nebrija and Gainza. Add to this severity that in my judgement was exaggerated and you have a picture,
+          perhaps vague, that I have made of him, but I remember only this."
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          First School Brawl
+        </h6>
+        <p>
+          In the afternoon of his first day in school, when the teacher was having his siesta, Jose met the bully, Pedro.
+          He was angry at this bully for making fun of him during his conversation with the teacher in the morning. Jose challenged Pedro to a fight.
+          The latter readily accepted, thinking that he could easily beat the Calamba boy who was smaller and younger. The two boys wrestled
+          furiously in the classroom, much to the glee of their classmates. Jose, having learned the art of wrestling from his athletic Tio Manuel,
+          defeated the bigger boy. For this feat, he became popular among his classmates. After the class in the afternoon,
+          a classmate named Andres Salandanan challenged him to an arm-wrestling match. They went to a sidewalk of a house and
+          wrestled with their arms. Jose, having the weaker arm, lost and nearly cracked his head on the sidewalk.
+          In succeeding days he had other fights with the boys of Biñan. He was not quarrelsome by nature, but he never ran away from a fight.
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Best Student in School
+        </h6>
+        <p>
+          In academic studies, Jose beat all Biñan boys. He surpassed them all in Spanish, Latin, and other subjects.
+          Some of his older classmates were jealous of his intellectual superiority. They wickedly squealed to the teacher
+          whenever Jose had a fight outside the school, and even told lies to discredit him before the teacher’s eyes.
+          Consequently the teacher had to punish Jose. 
+        </p>
+      </div>
+
+      <div class="wrapper">
+        <h6>
+          Early Schooling in Biñan
+        </h6>
+        <p>
+          Jose had a very vivid imagination and a very keen sense of observation. At the age of seven he traveled with his father for
+          the first time to Manila and thence to Antipolo to fulfill the promise of a pilgrimage made by his mother at the time of his birth.
+          They embarked in a casco, a very ponderous vessel commonly used in the Philippines. It was the first trip on the lake
+          that Jose could recollect. As darkness fell he spent the hours by the katig, admiring the grandeur of the water and the
+          stillness of the night, although he was seized with a superstitious fear when he saw a water snake entwine itself around the
+          bamboo beams of the katig. With what joy did he see the sun at the daybreak as its luminous rays shone upon the glistening surface
+          of the wide lake, producing a brilliant effect! With what joy did he talk to his father, for he had not uttered a word during the night!
+        </p>
+        <p>
+          When they proceeded to Antipolo, he experienced the sweetest emotions upon seeing the gay banks of the Pasig and the towns of Cainta
+          and Taytay. In Antipolo he prayed, kneeling before the image of the Virgin of Peace and Good Voyage, of whom he would later sing
+          in elegant verses. Then he saw Manila, the great metropolis , with its Chinese sores and European bazaars. And visited his elder
+          sister, Saturnina, in Santa Ana, who was a boarding student in the Concordia College.
+        </p>
+        <p>
+          When he was nine years old, his father sent him to Biñan to continue studying Latin, because his first teacher had died.
+          His brother Paciano took him to Biñan one Sunday, and Jose bade his parents and sisters good-bye with tears in his eyes.
+          Oh, how it saddened him to leave for the first time and live far from his home and his family! But he felt ashamed to cry and
+          had to conceal his tears and sentiments. "O Shame," he explained, "how many beautiful and pathetic scenes the world
+          would witness without thee!"
+        </p>
+        <p>
+          They arrived at Biñan in the evening. His brother took him to the house of his aunt where he was to stay, and left him after
+          introducing him to the teacher. At night, in company with his aunt’s grandson named Leandro, Jose took a walk around the town
+          in the light of the moon. To him the town looked extensive and rich but sad and ugly.
+        </p>
+        <p>
+          His teacher in Biñan was a severe disciplinarian. His name was Justiniano Aquino Cruz. "He was a tall man, lean and long-necked,
+          with a sharp nose and a body slightly bent forward. He used to wear a sinamay shirt woven by the deft hands of Batangas women.
+          He knew by memory the grammars of Nebrija and Gainza. To this add a severity which, in my judgement I have made of him,
+          which is all I remember."
+        </p>
+        <p>
+          The boy Jose distinguished himself in class, and succeeded in surpassing many of his older classmates. Some of these were
+          so wicked that, even without reason, they accused him before the teacher, for which, in spite of his progress, he received
+          many whippings and strokes from the ferule. Rare was the day when he was not stretched on the bench for a whipping or punished
+          with five or six blows on the open palm. Jose’s reaction to all these punishments was one of intense resentment in order to
+          learn and thus carry out his father’s will.
+        </p>
+        <p>
+          Jose spent his leisure hours with Justiniano’s father-in-law, a master painter. From him he took his first two sons, two nephews,
+          and a grandson. His way life was methodical and well regulated. He heard mass at four if there was one that early,
+          or studied his lesson at that hour and went to mass afterwards. Returning home, he might look in the orchard for a mambolo fruit to eat,
+          then he took his breakfast, consisting generally of a plate of rice and two dried sardines. 
+        </p>
+        <p>
+          After that he would go to class, from which he was dismissed at ten, then home again. He ate with his aunt and then began at ten,
+          then home again. He ate with his aunt and then began to study. At half past two he returned to class and left at five.
+          He might play for a short time with some cousins before returning home. He studied his lessons, drew for a while, and then
+          prayed and if there was a moon, his friends would invite him to play in the street in company with other boys.
+        </p>
+        <p>
+          Whenever he remembered his town, he thought with tears in his eyes of his beloved father, his idolized mother,
+          and his solicitous sisters. Ah, how sweet was his town even though not so opulent as Biñan! He grew sad and thoughtful.
+        </p>
+        <p>
+          While he was studying in Biñan, he returned to his hometown now and then. How long the road seemed to him in going and
+          how short in coming! When from afar he descried the roof of his house, secret joy filled his breast. How he looked for
+          pretexts to remain longer at home! A day more seemed to him a day spent in heaven, and how he wept, though silently and secretly,
+          when he saw the calesa that was flower that him Biñan! Then everything looked sad; a flower that he touched, a stone that
+          attracted his attention he gathered, fearful that he might not see it again upon his return. It was a sad but delicate and
+          quite pain that possessed him. 
+        </p>
+      </div>
+
     </div>
   </div>
-
 </template>
