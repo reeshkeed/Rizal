bdbf5c48cfddb03ddf3c2c1849b6cb5c8dd2a683
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 02:47:55 +0800

    Working on sculptures page image loading problem

--- src/views/Sculptures.vue
+++ src/views/Sculptures.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sculptures">
-    <div class="jumbotron v-centered bg-sculptures">
+    <div class="jumbotron v-centered slant-left bg-sculptures">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Sculptures</h1>
@@ -8,10 +8,61 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+      <div class="wrapper text-center">
+        <h3>Some of Rizal's Sculptures</h3>
+      </div>
 
+      <div v-for="sculpture in sculptures">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card">
+              <img v-bind:src="sculpture.thumbnail" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">{{ sculpture.name }}</h5>
+                <p>
+                  {{ sculpture.material }}<br>
+                  {{ sculpture.remarks }}
+                </p>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-info" data-toggle="modal" v-bind:data-target="sculpture.buttonId">
+                  View Image
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" v-bind:id="sculpture.modalId" tabindex="-1" role="dialog" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <img v-bind:src="sculpture.image" class="card-img-top" alt="...">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
 </template>
+
+<style lang="sass">
+
+</style>
+
+<script>
+import sculptures from '../../static/json/sculptures.json'
+
+export default {
+  data: () => ({
+    sculptures
+  })
+}
+
+</script>
