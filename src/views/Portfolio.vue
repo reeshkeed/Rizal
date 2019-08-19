90656aa14db3158c0b5a93e1d9de8ec5d49bdc84
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 06:03:37 +0800

    Added portfolio page content

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="portfolio">
-    <div class="jumbotron v-centered bg-portfolio">
+    <div class="jumbotron v-centered slant-left bg-portfolio">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Portfolio</h1>
@@ -8,10 +8,50 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+      <div class="wrapper text-center">
+        <h3>Achievements</h3>
+      </div>
 
+      <div class="wrapper">
+        <ul>
+          <li>
+            He became the most intelligent student when he finished high school at Ateneo
+          </li>
+          <li>
+            Won a lot of literary contests during his school days.
+          </li>
+          <li>
+            He is a multi lingual man who can speak up to 22 languages.
+          </li>
+          <li>
+            He wrote 2 novels exposing the tyranny and abuses of the Spanish friars and some Spanish officials.
+          </li>
+          <li>
+            Annotated Morga’s book that describes the Filipino life has its own culture when the Spaniards arrived in the Philippines.
+          </li>
+          <li>
+            Wrote meaning articles in the anti Spanish friars newspaper La Solidaridad such as THE PHILIPPINES WITHIN A CENTURY
+            AND THE INDOLENCE OF THE FILIPINOS.
+          </li>
+          <li>
+            He became like an architect, planner and engineer during his exile in Dapitan.
+          </li>
+          <li>
+            He wrote poems and novels which the Filipinos, until now, read and study as part of the educational curriculum.
+          </li>
+          <li>
+            He studied and graduated abroad and became a doctor despite financial and possibly racial difficulties.
+          </li>
+          <li>
+            He fought, in his own way, for the country and stood for our Independence. While there may be arguments on how valid his works
+            and contributions are compared to other Filipino heroes who actually fought, the Spaniards at least thought Rizal, with his pen and intelligence, is enough threat to execute him.
+          </li>
+          <li>
+            Ultimately, he died fighting for our country’s independence. His death ignited the fire of the revolutionary army.
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
 </template>
