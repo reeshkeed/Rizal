87751b036fa360bde2916179ae04b1ab6fa8241e
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 18:25:00 +0800

    Added travels page content

--- src/views/Travels.vue
+++ src/views/Travels.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="travels">
-    <div class="jumbotron v-centered bg-travels">
+    <div class="jumbotron v-centered slant-left bg-travels">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Travels</h1>
@@ -8,10 +8,143 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+      <div class="wrapper">
+        <h6>May 3, 1882</h6>
+        <p>Rizal left Philippines to complete his studies in Spain. He boarded the Salvadora using a passport of the name Jose Mercado.</p>
 
+        <h3>Secret Mission</h3>
+        <ul>
+          <li>
+            Observe the life and culture, customs, governments and laws of European nations in order to prepare himself in the task of
+            liberating the Filipino people from Spanish tyranny.
+          </li>
+          <li>Rizal’s departure was kept secret to avoid detection from Spanish authorities and the friars.</li>
+        </ul>
+
+        <h3>Singapore</h3>
+        <h6>May 9, 1882</h6>
+        <p>The Salvadora docked at Singapore</p>
+        <p>
+          The first stop over of Rizal on his way to Spain is Singapore, which he spent for sightseeing the city including its famed Botanical Garden,
+          Buddhist temples, the monument of Sir Thomas Stanford Raffles, the founder of Singapore.
+        </p>
+
+        <h3>Colombo, Sri Lanka</h3>
+        <p>
+          It was Rizal’s journey to Colombo, Sri Lanka that was important for him to improve his knowledge of the French language while
+          on board a French ship, Djemnah.
+        </p>
+
+        <h3>Egypt</h3>
+        <p>
+          In route through the Suez Canal, Rizal got off at Red Sea terminal and was amazed by the impressive moonlight scenery in Suez and was engrossed
+          with the multicultural people and language of the place.
+        </p>
+
+        <h3>Naples, Italy</h3>
+        <h6>June 11, 1882</h6>
+        <p>Rizal went around the city of Naples. This was the first European ground he set foot on.</p>
+
+        <h3>Merseilles, France</h3>
+        <h6>June 13, 1882</h6>
+        <p>Rizal arrived early in the morning and boarded at the Noalles Hotel.</p>
+        <h6>June 15, 1882</h6>
+        <p>He left for Barcelona in an express train.</p>
+
+        <h3>Barcelona, Spain</h3>
+        <p>He wrote his first article (“Amor Patrio”) abroad. It was published in Diaryong Tagalog.</p>
+
+        <h3>Madrid, Spain</h3>
+        <p>
+          Rizal studied in University Central de Madrid. He then had a love affair with Consuelo Ortiga y Perez, the daughter of Don Pablo.
+        </p>
+
+        <h3>Paris</h3>
+        <p>
+          He stayed at the Hotel de Paris, but them moved to a cheaper hotel. He was amazed by the attractive scenery and he often visited museums.
+          After departure for Spain, things turned from bad to worse in Calamba.
+        </p>
+
+        <h3>Germany</h3>
+        <ul>
+          <li>He visited Paris and Germany to specialize in ophthalmology.</li>
+          <li>He stopped over at Barcelona, on his way to Paris, to visit his friend Maximo Viola.</li>
+          <li>
+            He met Dr. Feodor Jagor author of Travels in the Philippines, a book that Rizal admired because of its keen observances in the
+            Philippines setting.
+          </li>
+          <li>With the help of Maximo Viola, who gave him the necessary funds to publish the novel, Noli Me Tangere was published.</li>
+        </ul>
+
+        <h3>Switzerland</h3>
+        <ul>
+          <li>Accompanied by his friend Maximo Viola.</li>
+          <li>They visited Ferdinand Blumentritt.</li>
+          <li>Rizal and Viola arted ways in Geneva.</li>
+        </ul>
+
+        <h3>Italy</h3>
+        <ul>
+          <li>After a week of staying in Rome, he prepared to return to the Philippines.</li>
+          <li>He had already written to his father that he was coming home.</li>
+        </ul>
+
+        <h3>United States</h3>
+        <p>
+          He left New York for Liverpool on board the City of Rome. He was onboard in a steamer which was
+          “the second largest ship in the world” – The Great Eastern.
+        </p>
+
+        <h3>London</h3>
+        <ul>
+          <li>London was a safe place from the attacks of Spanish tyranny.</li>
+          <li>He was a boarder of the Beckett family.</li>
+          <li>Fell in love with Gertrude Beckett.</li>
+          <li>Rizal also wrote his first article in La Solidaridad, a patriotic newspaper founded by Graciano Lopez Jaena.</li>
+          <li>
+            Society founded by Rizal in Paris was the R.D.L.M. Society. The aim of the secret society is the propagation of all useful
+            knowledge in the Philippines. Another aims is the redemption of the Malay race.
+          </li>
+        </ul>
+
+        <h3>Belgium</h3>
+        <ul>
+          <li>In Brussels Rizal was busy writing his second novel “El Filibusterismo”.</li>
+          <li>
+            The Calamba agrarian trouble was getting worse. The management of the Dominican hacienda continually raised the land rents.
+          </li>
+        </ul>
+
+        <h3>Madrid</h3>
+        <ul>
+          <li>Rizal sought the help of the Filipino colony to protest the injustices of the Dominicans.</li>
+          <li>His life was full of misfortunes.</li>
+          <li>He further learned from Saturnina’s letter that their parents had been forcibly ejected from their home.</li>
+        </ul>
+
+        <h3>Biarritz</h3>
+        <ul>
+          <li>Rizal took a vacation in the resort city of Biarritz on the fabulous French Riviera. He was the guest of the Boustead family.</li>
+          <li>He had finished the last chapter of his second novel, El Filibusterismo.</li>
+          <li>Rizal was in-love with Nellie Boustead, a daughter of the Boustead family.</li>
+        </ul>
+
+        <h3>Ghent</h3>
+        <ul>
+          <li>July 5, 1891 - Rizal left Brussels for Ghent a famous university city in Belguim.</li>
+          <li>He stayed at Ghent because the cost of printing is cheaper.</li>
+          <li>After the publication of El Filibusterismo, Rizal left Europe for Hong Kong.</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<style lang="sass">
+.wrapper h3, .wrapper h6
+  font-weight: 500
+
+.wrapper h6
+  margin: 1rem 0
+</style>
