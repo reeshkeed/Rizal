5145a8d3009324deb1433fe53adaa6a2980885d6
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 17:55:06 +0800

    Added lovelife page content

--- src/views/Lovelife.vue
+++ src/views/Lovelife.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lovelife">
-    <div class="jumbotron v-centered bg-lovelife">
+    <div class="jumbotron v-centered slant-left bg-lovelife">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">Lovelife</h1>
@@ -8,10 +8,200 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+      <div class="wrapper text-center">
+        <h3>Rizal's Lovelife</h3>
+        <p>
+          There were at least nine women linked with Rizal; namely SegundaKatigbak, LeonorValenzuela,Leonor Rivera, Consuelo Ortiga,
+          O-Sei San, Gertrude Beckette, Nelly Boustead, Suzanne Jacoby and Josephine Bracken. These women might have been beguiled
+          by his intelligence, charm and wit.
+        </p>
+      </div>
 
+      <div class="wrapper">
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/segunda.jpg" class="card-img" alt="Segunda Katigbak">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Segunda Katigbak</h5>
+                <p class="card-text">
+                  Segunda Katigbak was her puppy love. Unfortunately, his first love was engaged to be married to a town mate- Manuel Luz.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/leonor.jpg" class="card-img" alt="Leonor Valenzuela">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Leonor Valenzuela</h5>
+                <p class="card-text">
+                  Leonor Valenzuela, a tall girl from Pagsanjan. Rizal send her love notes written in invisible ink that could only be
+                  deciphered over the warmth of the lamp or candle. He visited her on the eve of his departure to Spain and bade her a last goodbye.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/rivera.jpg" class="card-img" alt="Leonor Rivera">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Leonor Rivera</h5>
+                <p class="card-text">
+                  Leonor Rivera, his sweetheart for 11 years played the greatest influence in keeping him from falling in love with other women
+                  during his travel. Unfortunately, Leonor’s mother disapproved of her daughter’s relationship with Rizal, who was then a known
+                  filibustero. She hid from Leonor all letters sent to her sweetheart. Leonor believing that Rizal had already forgotten her,
+                  sadly consented her to marry the Englishman Henry Kipping, her mother’s choice.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/consuelo.jpg" class="card-img" alt="Consuelo Ortiga">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Consuelo Ortiga</h5>
+                <p class="card-text">
+                  Consuelo Ortiga y Rey, the prettier of Don Pablo Ortiga’s daughters, fell in love with him. He dedicated to her A la Senorita C.O. y R.,
+                  which became one of his best poems. The Ortiga's residence in Madrid was frequented by Rizal and his compatriots. He probably
+                  fell in love with her and Consuelo apparently asked him for romantic verses. He suddenly backed out before the relationship
+                  turned into a serious romance, because he wanted to remain loyal to Leonor Rivera and he did not want to destroy his friendship
+                  with Eduardo de Lete who was madly in love with Consuelo.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/oseisan.jpg" class="card-img" alt="O Sei San">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">O Sei San</h5>
+                <p class="card-text">
+                  O Sei San, a Japanese samurai’s daughter taught Rizal the Japanese art of painting known as su-mie. She also helped Rizal improve
+                  his knowledge of Japanese language. If Rizal was a man without a patriotic mission, he would have married this lovely
+                  and intelligent woman and lived a stable and happy life with her in Japan because Spanish legation there offered him a lucrative job.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/beckett.jpg" class="card-img" alt="Gertrude Beckett">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Gertrude Beckett</h5>
+                <p class="card-text">
+                  While Rizal was in London annotating the Sucesos de las Islas Filipinas, he boarded in the house of the Beckett family,
+                  within walking distance of the British Museum. Gertrude, a blue-eyed and buxom girl was the oldest of the three Beckett daughters.
+                  She fell in love with Rizal. Tottie helped him in his painting and sculpture. But Rizal suddenly left London for Paris
+                  to avoid Gertrude, who was seriously in love with him. Before leaving London, he was able to finish the group carving of the
+                  Beckett sisters. He gave the group carving to Gertrude as a sign of their brief relationship.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/nellie.jpg" class="card-img" alt="Nellie Boustead">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Nellie Boustead</h5>
+                <p class="card-text">
+                  Rizal having lost Leonor Rivera, entertained the thought of courting other ladies. While a guest of the Boustead family at
+                  their residence in the resort city of Biarritz, he had befriended the two pretty daughters of his host, Eduardo Boustead.
+                  Rizal used to fence with the sisters at the studio of Juan Luna. Antonio Luna, Juan’s brother and also a frequent visitor of the
+                  Bousteads, courted Nellie but she was deeply infatuated with Rizal. In a party held by Filipinos in Madrid, a drunken Antonio Luna
+                  uttered unsavory remarks against Nellie Boustead. This prompted Rizal to challenge Luna into a duel. Fortunately, Luna apologized
+                  to Rizal, thus averting tragedy for the compatriots.
+                  <br><br>
+                  Their love affair unfortunately did not end in marriage. It failed because Rizal refused to be converted to the Protestant faith,
+                  as Nellie demanded and Nellie’s mother did not like a physician without enough paying clientele to be a son-in-law. The lovers,
+                  however, parted as good friends when Rizal left Europe.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/jacoby.jpg" class="card-img" alt="Suzanne Jacoby">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Suzanne Jacoby</h5>
+                <p class="card-text">
+                  In 1890, Rizal moved to Brussels because of the high cost of living in Paris. In Brussels, he lived in the boarding house
+                  of the two Jacoby sisters. In time, they fell deeply in love with each other. Suzanne cried when Rizal left Brussels and wrote
+                  him when he was in Madrid.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3">
+          <div class="row no-gutters">
+            <div class="col-md-3">
+              <img src="../../static/img/lovelife/bracken.jpg" class="card-img" alt="Josephine Bracken">
+            </div>
+            <div class="col-md v-centered">
+              <div class="card-body">
+                <h5 class="card-title">Josephine Bracken</h5>
+                <p class="card-text">
+                  In the last days of February 1895, while still in Dapitan, Rizal met an 18-year old petite Irish girl, with bold blue eyes,
+                  brown hair and a happy disposition. She was Josephine Bracken, the adopted daughter of George Taufer from Hong Kong,
+                  who came to Dapitan to seek Rizal for eye treatment. Rizal was physically attracted to her. His loneliness and boredom must
+                  have taken the measure of him and what could be a better diversion that to fall in love again. But the Rizal sisters suspected
+                  Josephine as an agent of the friars and they considered her as a threat to Rizal’s security.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
 </template>
+
+<style lang="sass">
+.card
+  border: 0
+  margin-bottom: 3rem !important
+
+.card-img
+  border-radius: 100%
+
+@media (max-width: 767px)
+  .card-img
+    width: 50%
+
+  .no-gutters
+    text-align: center
+</style>
