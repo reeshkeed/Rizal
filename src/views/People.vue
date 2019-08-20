265b2eb5ab266bf6d24cdeaa0e86bd592bc1d902
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 19:31:46 +0800

    Added people page content

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="people">
-    <div class="jumbotron v-centered bg-people">
+    <div class="jumbotron v-centered slant-left bg-people">
       <div class="jumbotron-overlay"></div>
       <div class="container">
         <h1 class="display-4 text-center">People</h1>
@@ -8,10 +8,276 @@
     </div>
 
     <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+      <div class="wrapper text-center">
+        <h3>Different People Encountered by Rizal</h3>
+      </div>
 
+      <div class="wrapper">
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Spain</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Paterno Brothers ( Antonio, Pedro and Maximo)</td>
+              <td>Friends of Rizal who shared his ideas. They often held meeting and celebrations in their home.</td>
+            </tr>
+            <tr>
+              <td>Don Pablo Ortega y Rey</td>
+              <td>A rich mestizo who was a former mayor of Manila. Befriended Rizal due to him being in love with her daughter.</td>
+            </tr>
+            <tr>
+              <td>Consuelo Ortega y Rey</td>
+              <td>One of the many lovers of Rizal. Very beautiful and educated and further described in Rizal's poem dedicated to her.</td>
+            </tr>
+            <tr>
+              <td>Senor Roses</td>
+              <td>A bookstore owner whom Rizal frequently buys from.</td>
+            </tr>
+            <tr>
+              <td>Suzanne Jacoby</td>
+              <td>Another lover of Rizal while in Spain. Beautiful and pretty as described by him, confessed her love as he left Spain for France.</td>
+            </tr>
+            <tr>
+              <td>Marcelo H. Del Pilar</td>
+              <td>An Illustrado and a leading figure of the propaganda movement. Founder of the famous La Solidaridad and a respected member of the Freemasons.</td>
+            </tr>
+            <tr>
+              <td>Mariano Ponce</td>
+              <td>Another famous figure in the propaganda movement. Also the co ± founder of La Solidaridad together with Marcelo del Pilar.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">France</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Dr. Niscase</td>
+              <td>A famous Parisian doctor. Rizal studied his works and also worked for him.</td>
+            </tr>
+            <tr>
+              <td>Maximo Viola</td>
+              <td>A very good friend of Rizal. The saviour of Noli Me Tangere because he helped Rizal when he became near destitute.</td>
+            </tr>
+            <tr>
+              <td>Dr. Louis de Weckert</td>
+              <td>A leading French ophthalmologist. Rizal worked for him as an assistant.</td>
+            </tr>
+            <tr>
+              <td>Paz Pardo de Tavera</td>
+              <td>A pretty girl and fiancée to his good friend, Juan Luna.</td>
+            </tr>
+            <tr>
+              <td>Nellie Boustead</td>
+              <td>A beautiful Parisian. The very girl who became the reason for the momentary rivalry between Rizal and Antonio Luna; 
+                the famous Revolutionary General.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Germany</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Dr. Otto Becker</td>
+              <td>Another well known ophthalmologist. Rizal worked for him as an assistant.</td>
+            </tr>
+            <tr>
+              <td>Dr. Karl Ullmer</td>
+              <td>A kind protestant pastor. Rizal stayed at his home and became best of friends and became his admirer.</td>
+            </tr>
+            <tr>
+              <td>Prof. Frederich Ratzel</td>
+              <td>A famous German historian. Rizal befriended him.</td>
+            </tr>
+            <tr>
+              <td>Dr. Adolf B. Meyer</td>
+              <td>He is the Director of the Anthropological Museum. Also befriended by Rizal.</td>
+            </tr>
+            <tr>
+              <td>Dr. Feodor Jagor</td>
+              <td>A celebrated scientist-traveller. Author of the book: Travels in the Philippines which Rizal read as a child.</td>
+            </tr>
+            <tr>
+              <td>Dr. Hans Virchow</td>
+              <td>Introduced to Rizal by Dr. Jagor. He is a famous German anthropologist.</td>
+            </tr>
+            <tr>
+              <td>Dr. Ernest Schwelgger</td>
+              <td>A German ophthalmologist. Rizal worked for him too.</td>
+            </tr>
+            <tr>
+              <td>Madame Lucle Cerdole</td>
+              <td>Became Rizal's professor in French.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Bohemia</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Prof. Blumentritt</td>
+              <td>A kind-hearted Austrian professor. Develpoed a lifelong friendship with Rizal.</td>
+            </tr>
+            <tr>
+              <td>Burgo Master</td>
+              <td>A kind town Mayor who welcomed Rizal in open arms.</td>
+            </tr>
+            <tr>
+              <td>Dr. Carlos Czepelak</td>
+              <td>A renowned scientist in Europe. He met Rizal at Leitmeritz.</td>
+            </tr>
+            <tr>
+              <td>Prof. Robert Klutschak</td>
+              <td>An eminent naturist. Also met Rizal in Leitmeritz.</td>
+            </tr>
+            <tr>
+              <td>Dr. Willkomm</td>
+              <td>A professor of natural history in the University of Prague.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Philippines (Back)</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Gov. Gen. Emilio Terrero</td>
+              <td>A liberal Gov. Gen. who defended Rizal from the friars because of the controversy caused
+                by his novel; Noli Me Tangere and advised him to leave the Philippines.</td>
+            </tr>
+            <tr>
+              <td>Don Jose Luis Taviel de Andrade</td>
+              <td>A very good friend of Rizal. He became his lawyer at the same time his bodyguard.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Hong Kong</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jose Maria Basa</td>
+              <td>A Filipino resident in hong Kong. He also toured Rizal in Macau.</td>
+            </tr>
+            <tr>
+              <td>Balbino Mauricio</td>
+              <td>Another Filipino resident whom Rizal befriended while in Hong Kong.</td>
+            </tr>
+            <tr>
+              <td>Manuel Yriate</td>
+              <td>Also a Filipino resident in Hong Kong. He is the son of the alcalde mayor in Laguna.</td>
+            </tr>
+            <tr>
+              <td>Jose Slanz de Varanda</td>
+              <td>A Spaniard who served as a secretary of Gov. Gen. Terrero and became a spy by shadowing Rizal's movements in Hong Kong.</td>
+            </tr>
+            <tr>
+              <td>Josephine Bracken</td>
+              <td>Another well known lover of Rizal. She almost became her wife had he was not afraid of commitment.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Japan</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Seiko Usul</td>
+              <td>A lover of Rizal, very beautiful Japanese girl. She taught him Japanese language which he mastered within a week. 
+                Almost got Rizal to settle in Japan.</td>
+            </tr>
+            <tr>
+              <td>Tetcho Suehiro</td>
+              <td>A fighting Japanese journalist who was forced into exile by the government. Met Rizal on the
+                Belgic and became friends.</td>
+            </tr>
+            <tr>
+              <td>Juan Perez Caballero</td>
+              <td>The secretary of the Spanish Legation in Hong Kong and invited Rizal to reside in the Spanish Legation.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">London</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Dr. Antonio Ma. Regidor</td>
+              <td>A practicing lawyer where Rizal stayed at his home as a guest.</td>
+            </tr>
+            <tr>
+              <td>Dr. Relnhold Rozt</td>
+              <td>A librarian of the Ministry of Foreign Affairs. Recommended Rizal to the authorities of the
+                British Museum and famously called him a pearl of a man.</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table table-responsive-sm table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th class="first-col" scope="col">Macau</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Don Juan Francisco Lecaros</td>
+              <td>A Filipino gentleman, who accommodated Rizal and Basa at his home.</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<style lang="sass">
+.table
+  margin-bottom: 3rem
+
+.first-col
+  width: 30%
+</style>
