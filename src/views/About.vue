fc76a51c4e7962a4e60d1361ea78368946c0553e
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 20:15:45 +0800

    Added about page content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,79 @@
 <template>
   <div class="about">
-    <div class="jumbotron v-centered bg-about">
+    <div class="jumbotron v-centered slant-left bg-about">
       <div class="jumbotron-overlay"></div>
       <div class="container">
-        <h1 class="display-4 text-center">About Us</h1>
+        <h1 class="display-4 text-center">About</h1>
       </div>
     </div>
 
-    <div class="container">
-      <h2 class="sub-title text-center">
-        Subtitle
-      </h2>
-    </div>
-  </div>
+    <div class="container text-center">
+      <div class="wrapper">
+        <h3>About This Website</h3>
+        <p>
+          As completing a subject we are required to create a website about Rizal. This website contains information about Jose Rizal's life and works.
+          It aims to help our fellow students and other people who need or having a research about Rizal's life.
+        </p>
+        <p>
+          It contains the details of his different characteristics, his family, his travels abroad to study, his poems, philosophies, achievements,
+          and other works and accomplishments.
+        </p>
+        <p>
+          Through this website, you will learn the importance of learning the life and works of Rizal.
+        </p>
+      </div>
 
+      <div class="wrapper">
+        <h3>Credits</h3>
+
+        <h5>Research Team</h5>
+
+        <h6>Canlas, Paul Kristian</h6>
+        <p>Lovelife and Education of Rizal</p>
+        <h6>Caravana, Oliver</h6>
+        <p>Quotes and Trivia</p>
+        <h6>Caranay, Conrad</h6>
+        <p>Personalities, Achievement's, Foods, People</p>
+        <h6>De leon, Neil Patrick</h6>
+        <p>Childhood and Family of Rizal</p>
+        <h6>Dungca, Gerald Kiel</h6>
+        <p>Travels and Philosophies of Rizal</p>
+        <h6>Exciomo, John Rudy</h6>
+        <p>Works</p>
+
+        <h5>Dev Team</h5>
+        <h6>Daloso, Girish</h6>
+        <p>Lead Programmer</p>
+        <h6>Collado, Rowel</h6>
+        <p>Analyst</p>
+
+        <h3>References</h3>
+        <a href="http://thelifeandworksofrizal.blogspot.com/2011/12/childhood-in-calamba.html">Childhood of Rizal</a><br>
+        <a href="http://joserizal.ph/ec01.html">Calamba, Laguna</a><br>
+        <a href="http://joserizal.ph/fm01.html">Family of Rizal</a><br>
+        <a href="https://www.scribd.com/doc/19375852/Family-Tree-of-Rizal">Family Tree</a><br>
+        <a href="http://www.joserizal.ph/ed01.html">Education</a><br>
+        <a href="http://www.joserizal.ph/ph01.html">Philosophies</a><br>
+        <a href="http://www.joserizal.ph/im07.html">Sculptures</a><br>
+        <a href="http://www.joserizal.ph/pm01.html">Poems</a><br>
+        <a href="http://www.joserizal.ph/pa01.html">Paintings</a><br>
+        <a href="http://www.joserizal.ph/pa01.html">Paintings</a><br>
+        <a href="https://uixccelerate.wordpress.com/2011/06/20/vexel-and-vector-ph-celebrates-dr-jose-rizals-150th-birth-anniversary/">Rizal logo icon (Edited)</a><br>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<style scoped lang="sass">
+  .wrapper h3
+    margin-bottom: 2rem
+
+  .wrapper h5
+    font-size: 1.2rem
+    margin: 2rem 0
+    color: #3097D1
+
+  .wrapper h6
+    margin: 2rem 0 0 0
+</style>
