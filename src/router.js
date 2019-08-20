db83616786882fd14444b26d66fb65127fe7932e
reeshkeed <girishdaloso@hotmail.com>
2019-08-20 20:41:20 +0800

    Added on change page scroll up effect

--- src/router.js
+++ src/router.js
@@ -110,5 +110,40 @@ export default new Router({
       component: About,
       meta: { showFooter: true }
     }
-  ]
+  ],
+
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    return new Promise((resolve, reject) => {
+      let scrollDuration = 500,
+          cosParameter = window.scrollY / 2,
+          oldTimestamp = performance.now(),
+          scrollCount = 0
+
+      function step (newTimestamp) {
+        scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp))
+
+        if (scrollCount >= Math.PI) {
+          window.scrollTo(0, 0)
+        }
+
+        if (window.scrollY === 0) {
+          resolve({ x: 0, y: 0 })
+
+          return
+        }
+
+        window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)))
+
+        oldTimestamp = newTimestamp
+
+        window.requestAnimationFrame(step)
+      }
+
+      window.requestAnimationFrame(step);
+    })
+  }
 })
